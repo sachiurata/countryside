fe45df1f564a92c99ea14f5178b2f4a0d3217ed1
sachi <urata986@gmail.com>
2023-06-22 00:16:09 +0000

    Deviseのlast_sign_in_atを設定する途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,12 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
   
   include ApplicationHelper
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:last_sign_in_at])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -97,6 +97,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_20_004150) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "last_sign_in_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
