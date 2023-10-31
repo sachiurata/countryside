98e01b9e79ac153bba85390803091fdf439a0e40
sachiurata <urata986@gmail.com>
2023-10-31 07:08:44 +0000

    posts/showのレイアウト変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,8 @@ class HomeController < ApplicationController
   def index
     popular_posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
     @posts = popular_posts - @non_public_posts
-    @posts = @posts.first(3)
+    @posts = @posts
+    # @posts = @posts.first(3)
     @articles = Article.order("updated_at DESC").first(3)
   end
 end
