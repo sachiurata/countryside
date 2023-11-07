7ec6722dfa8413f59dbf0acfc643acb2324011d4
sachiurata <urata986@gmail.com>
2023-11-07 02:46:52 +0000

    user_profileのパーシャルを整理

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,11 @@ class HomeController < ApplicationController
   before_action :non_public_posts
   
   def index
-    popular_posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
-    @posts = popular_posts - @non_public_posts
-    @posts = @posts
+    # popular_posts = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+    # @posts = popular_posts - @non_public_posts
+    # @posts = @posts
     # @posts = @posts.first(3)
+    @posts = Post.all
     @articles = Article.order("updated_at DESC").first(3)
   end
 end
